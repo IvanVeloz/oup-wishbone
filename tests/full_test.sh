5df20f1025eab486d1edc57f3b90c98c11820eed
Ivan Veloz <devel@ivanveloz.com>
2024-01-01 17:54:26 -0500

    Added fulltest.sh

---
+++ tests/full_test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Full build and test
+
+set -e # script will fail if any command fails
+
+CALLINGDIR=$(pwd)
+BASEDIR=$(dirname "$0")
+REPOROOT=$(realpath ${REPOROOT:=${BASEDIR}/..})
+
+cd ${REPOROOT}
+
+echo "Initializing repositories"
+git submodule update --init --recursive
+
+echo "Making wbgen2"
+cd ${REPOROOT}/tools/wishbone-gen
+make
+
+echo "Making oup-wishbone"
+cd ${REPOROOT}
+make
+
+echo "Verifying output hash"
+tests/verify_output_up_to_date.sh
+
+echo "Done."
+cd ${REPOROOT}
